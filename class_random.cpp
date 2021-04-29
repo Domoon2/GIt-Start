af12831da946b70417129610d5f4c102ddc228a4
Domoon2 <38396675+Domoon2@users.noreply.github.com>
2021-04-29 15:30:03 +0900

    Add files via upload

---
+++ class_random.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class RandomInteger
+{
+private:
+	int low;
+	int high;
+	int value;
+public:
+	RandomInteger(int low, int high); //»ý¼ºÀÚ
+	~RandomInteger(); //¼Ò¸êÀÚ
+
+	RandomInteger(const RandomInteger& random) = delete;
+	void print() const;
+};
+
+RandomInteger::RandomInteger(int lw, int hh)
+:low(lw), high(hh)
+{
+	srand(time(0));
+	int temp = rand();
+	value = temp % (high - low + 1) + low;
+}
+RandomInteger::~RandomInteger()
+{
+
+}
+void RandomInteger::print() const
+{
+	cout << value << endl;
+}
+
+int main()
+{
+	RandomInteger r1(150, 300);
+	cout << "150~300 ·£´ý ¼ýÀÚ :";
+	r1.print();
+
+	RandomInteger r2(150, 3000);
+	cout << "0~3000 ·£´ý ¼ýÀÚ";
+	r2.print();
+
+	return 0;
+}
